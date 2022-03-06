9fda4f187c69b7f86d5f48a6c29855f3d790f672
Tanner Peterson <tannermp23@hotmail.com>
2022-03-05 21:40:29 -0800

    added comments

--- aiomp.py
+++ aiomp.py
@@ -4,6 +4,10 @@ from queue import Empty
 
 
 async def handle_user_input(subprocess_input_queue):
+    """Wait for user input and queue a command if applicable.
+
+    Entering 'quit' will break the loop and send queue tombstone message.
+    """
     while True:
         user_input = input("enter: ")
         if user_input == "quit":
@@ -24,6 +28,7 @@ async def handle_user_input(subprocess_input_queue):
 
 
 async def timer(time_remaining):
+    """Start a countdown timer with the given number of seconds"""
     while time_remaining > 0:
         print(time_remaining)
         await asyncio.sleep(1)
@@ -32,14 +37,25 @@ async def timer(time_remaining):
 
 
 async def handle_command(input_queue):
+    """Wait for a command in the queue and then process it.
+
+    Returns new tasks which should be awaited together.
+    Includes itself in tasks unless quitting so that the next
+    command can be processed.
+    """
     tasks = set()
 
+    # Regular mp queue is not awaitable and thus will block, so need to poll it then
+    # sleep to prevent blocking other coroutines. Should replace this with a
+    # process/thread safe + asyncio compatible queue
     command_dict = None
     while command_dict is None:
         try:
             command_dict = input_queue.get_nowait()
         except Empty:
             await asyncio.sleep(0.01)
+
+    # process command
     command = command_dict["command"]
     if command == "quit":
         return set()
@@ -50,11 +66,17 @@ async def handle_command(input_queue):
     else:
         print(f"unrecognized command: {command}")
 
+    # add this function to new tasks so that another command can be processed
     tasks.add(asyncio.create_task(handle_command(input_queue)))
+
     return tasks
 
 
 async def subprocess_main(input_queue):
+    """Process all tasks until there are no more pending completion.
+
+    Completed tasks may result in one or many new tasks being created.
+    """
     pending = {asyncio.create_task(handle_command(input_queue))}
     while True:
         done, pending = await asyncio.wait(pending, return_when=asyncio.FIRST_COMPLETED)
@@ -77,5 +99,6 @@ async def main():
 
 
 if __name__ == "__main__":
+    # not sure if start method must be 'spawn' for this simple non-frozen example code
     aiomultiprocess.set_start_method("spawn")
     asyncio.run(main())
